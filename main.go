bbf7d4562f0a5b8cdf144a0a0e4a37ca0211131a
Henrique Knack <murilo.012@edu.pucrs.br>
2024-09-24 14:31:48 -0300

    solução sequencial para o problema dos filosofos

--- main.go
+++ main.go
@@ -1,89 +1,59 @@
-//concorente
 package main
 
 import (
 	"fmt"
 	"strings"
-	"sync"
 	"time"
 )
 
 const hunger = 3
 
 var philosophers = []string{"Platão", "Socrates", "Aristoteles", "Pascal", "Locke"}
-var wg sync.WaitGroup
-var sleepTime = 1 * time.Second
-var eatTime = 2 * time.Second
-var thinkTime = 1 * time.Second
 var orderFinished = []string{}
-var orderMutex sync.Mutex
 
-func diningProblem(p string, leftFork, rightFork *sync.Mutex) {
-	defer wg.Done()
-	fmt.Println(p, "esta sentado.")
-	// filosofos demoram 1seg para sentar.
-	time.Sleep(sleepTime)
+func diningProblem(p string) {
+	fmt.Println(p, "está sentado.")
+	// O filósofo leva um segundo para se sentar.
+	time.Sleep(1 * time.Second)
 
-	// cada filosofo vai comer 3x.
+	// Cada filósofo vai comer três vezes.
 	for i := hunger; i > 0; i-- {
-		fmt.Println(p, "esta com fome.")
-		// esta pegando os garfos, demorando 1seg
-		time.Sleep(sleepTime)
-		
-		// filosofo pegou o garfo esquerdo. garfo esquerdo travado. ninguem mais usa, ate ser desbloqueado.
-		leftFork.Lock()
-		fmt.Printf("\t%s pegou o garfo a sua esquerda.\n", p)
+		fmt.Println(p, "está com fome.")
+		// O filósofo pega ambos os garfos. Isso leva um segundo.
+		time.Sleep(1 * time.Second)
 
-		// filosofo pegou o garfo direito. garfo direito travado. ninguem mais usa, ate ser desbloqueado.
-		rightFork.Lock()
-		fmt.Printf("\t%s pegou o garfo a sua direita.\n", p)
+		fmt.Printf("\t%s pegou o garfo à sua esquerda.\n", p)
+		fmt.Printf("\t%s pegou o garfo à sua direita.\n", p)
 
-		fmt.Println(p, "esta com os dois garfos e comendo.")
-		// filosofo comeca a comer, 2seg para comer.
-		time.Sleep(eatTime)
+		fmt.Println(p, "está com os dois garfos e comendo.")
+		// O filósofo começa a comer. Isso leva dois segundos.
+		time.Sleep(2 * time.Second)
 
-		// dar temo para o filosofo pensar
-		fmt.Println(p, "esta pensando.")
-		time.Sleep(thinkTime)
+		fmt.Println(p, "está pensando.")
+		// Dê ao filósofo um tempo para pensar.
+		time.Sleep(1 * time.Second)
 
-		rightFork.Unlock()
 		fmt.Printf("\t%s devolveu o garfo direito.\n", p)
-		leftFork.Unlock()
 		fmt.Printf("\t%s devolveu o garfo esquerdo.\n", p)
-		// filosofo demora 1seg para botar os garfos na mesa 
-		time.Sleep(sleepTime)
+		// O filósofo leva um segundo para colocar ambos os garfos de volta.
+		time.Sleep(1 * time.Second)
 	}
 
-	// filosofo atual comeu
-	fmt.Println(p, "esta satisfeito.")
-	// filosofo saindo, demora 1seg
-	time.Sleep(sleepTime)
+	// O filósofo atual terminou de comer.
+	fmt.Println(p, "está satisfeito.")
+	// O filósofo está saindo. Leva um segundo.
+	time.Sleep(1 * time.Second)
 	fmt.Println(p, "saiu da mesa.")
-	orderMutex.Lock()
-	defer orderMutex.Unlock()
 	orderFinished = append(orderFinished, p)
 }
 
 func main() {
-	fmt.Println("O problema do jantar dos filosofos")
+	fmt.Println("O problema do jantar dos filósofos")
 	fmt.Println("-------------------------------")
 
-	wg.Add(len(philosophers))
-
-	forkLeft := &sync.Mutex{}
-
-	for i := 0; i < len(philosophers); i++ {
-		// mutex para o garfo direito
-		forkRight := &sync.Mutex{}
-		go diningProblem(philosophers[i], forkLeft, forkRight)
-
-		forkLeft = forkRight
-
-		// dormir 5seg para o proximo filosofo
-		time.Sleep(5 * time.Second)
+	for _, philosopher := range philosophers {
+		diningProblem(philosopher)
 	}
-
-	wg.Wait()
 
 	fmt.Println("Mesa vazia.")
 	fmt.Println("--------------------------")
